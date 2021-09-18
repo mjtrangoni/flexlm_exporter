baad1511b31918416ec699a64ba053b634dc6d2e
Mario Trangoni <mjtrangoni@gmail.com>
2021-09-18 18:33:45 +0200

    chore: Add golang v1.17, remove v1.16
    Signed-off-by: Mario Trangoni <mjtrangoni@gmail.com>

--- collector/lmstat.go
+++ collector/lmstat.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build linux || windows
 // +build linux windows
 
 package collector
